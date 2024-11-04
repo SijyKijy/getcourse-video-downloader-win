6ee85af6228e85dfd5b84b02bf2350c6ff03b96a
SijyKijy <SijyKijy@users.noreply.github.com>
2024-11-05 00:32:06 +0300

    Update logic

--- internal/utils/utils.go
+++ internal/utils/utils.go
@@ -6,10 +6,16 @@ import (
 	"log"
 	"os"
 	"os/exec"
+	"runtime"
 )
 
 func ClearScreen() {
-	cmd := exec.Command("clear")
+	var cmd *exec.Cmd
+	if runtime.GOOS == "windows" {
+		cmd = exec.Command("cmd", "/c", "cls")
+	} else {
+		cmd = exec.Command("clear")
+	}
 	cmd.Stdout = os.Stdout
 	err := cmd.Run()
 	if err != nil {
