f187699adde04f2b1fa609e614aaca03d8e66063
SijyKijy <SijyKijy@users.noreply.github.com>
2024-11-04 23:36:52 +0300

    Fix

--- internal/combiner/combiner.go
+++ internal/combiner/combiner.go
@@ -1,57 +1,56 @@
 package combiner
 
 import (
-	"fmt"
-	"log"
-	"os"
-	"os/exec"
-	"path/filepath"
-	"sort"
-	"strings"
+    "fmt"
+    "log"
+    "os"
+    "os/exec"
+    "path/filepath"
+    "sort"
 )
 
 func CombineFiles(outputFile string) error {
-	listFile, err := os.Create("ffmpeg_list_all.txt")
-	if err != nil {
-		return err
-	}
-	defer func(name string) {
-		err := os.Remove(name)
-		if err != nil {
-			log.Printf("Error removing file: %v", err)
-		}
-	}(listFile.Name())
+    listFile, err := os.Create("ffmpeg_list_all.txt")
+    if (err != nil) {
+        return err
+    }
+    defer func(name string) {
+        err := os.Remove(name)
+        if err != nil {
+            log.Printf("Error removing file: %v", err)
+        }
+    }(listFile.Name())
 
-	tsFiles, _ := filepath.Glob("parts/*.ts")
-	binFiles, _ := filepath.Glob("parts/*.bin")
-	files := append(tsFiles, binFiles...)
+    tsFiles, _ := filepath.Glob("parts/*.ts")
+    binFiles, _ := filepath.Glob("parts/*.bin")
+    files := append(tsFiles, binFiles...)
 
-	sort.Strings(files)
+    sort.Strings(files)
 
-	for _, file := range files {
-		// Convert the file path to a format with forward slashes
-		unifiedPath := filepath.ToSlash(file)
-		_, err := listFile.WriteString(fmt.Sprintf("file '%s'\n", unifiedPath))
-		if err != nil {
-			return err
-		}
-	}
-	err = listFile.Close()
-	if err != nil {
-		log.Printf("Error closing file: %v", err)
-	}
+    for _, file := range files {
+        // Convert the file path to a format with forward slashes
+        unifiedPath := filepath.ToSlash(file)
+        _, err := listFile.WriteString(fmt.Sprintf("file '%s'\n", unifiedPath))
+        if err != nil {
+            return err
+        }
+    }
+    err = listFile.Close()
+    if err != nil {
+        log.Printf("Error closing file: %v", err)
+    }
 
-	cmd := exec.Command("ffmpeg",
-		"-f", "concat",
-		"-safe", "0",
-		"-i", listFile.Name(),
-		"-c", "copy",
-		outputFile)
+    cmd := exec.Command("ffmpeg",
+        "-f", "concat",
+        "-safe", "0",
+        "-i", listFile.Name(),
+        "-c", "copy",
+        outputFile)
 
-	output, err := cmd.CombinedOutput()
-	if err != nil {
-		return fmt.Errorf("ffmpeg error: %v\nOutput: %s", err, string(output))
-	}
+    output, err := cmd.CombinedOutput()
+    if err != nil {
+        return fmt.Errorf("ffmpeg error: %v\nOutput: %s", err, string(output))
+    }
 
-	return nil
+    return nil
 }
